4e48de0b004a79bba57371668e62347325ef03db
MasterSSOA <tiburon.grande77@gmail.com>
2020-09-12 09:40:32 -0400

    Ejercicio 5 Finalizado.

--- Practica3/Practica3e5/Program.cs
+++ Practica3/Practica3e5/Program.cs
@@ -6,71 +6,99 @@
 //                                               © By Anthony Reyes Duran
 
 using System;
+using System.Text.RegularExpressions;
 
 namespace Practica3e5
 {
-    class Program
+    partial class Program
     {
         //Estructura principal
         static void Main(string[] args)
         {
+            string Mes;
+            int Captura;
+            try
+            {
+                Console.Write("CONVERTIDOR DE MES\n\nNúmero de Mes : ");
+                Captura = Convert.ToInt32(Console.ReadLine());
+                Mes = MonthConverter(Captura);
 
+                if (0 < Captura && Captura < 13)
+                    Console.WriteLine("\nEl Mes es " + Mes);
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error : " + e.Message.ToString()); 
+                Console.ReadKey();
+            }
         }
 
         //Función convertidor de número a nombre de mes.
-        static string MonthConverter(string Mes)
+        static string MonthConverter(int NumeroMes)
         {
-            Meses meses = Meses.Enero;
-
-            switch (meses)
+            string Mes = string.Empty;
+            
+            switch (NumeroMes)
             {
-                case Meses.Enero:
+                case 1:
+                    Mes = Meses.Enero.ToString();
                     break;
-                case Meses.Febrero:
+                case 2:
+                    Mes = Meses.Febrero.ToString();
                     break;
-                case Meses.Marzo:
+                case 3:
+                    Mes = Meses.Marzo.ToString();
                     break;
-                case Meses.Abril:
+                case 4:
+                    Mes = Meses.Abril.ToString();
                     break;
-                case Meses.Mayo:
+                case 5:
+                    Mes = Meses.Mayo.ToString();
                     break;
-                case Meses.Junio:
+                case 6:
+                    Mes = Meses.Junio.ToString();
                     break;
-                case Meses.Julio:
+                case 7:
+                    Mes = Meses.Julio.ToString();
                     break;
-                case Meses.Agosto:
+                case 8:
+                    Mes = Meses.Agosto.ToString();
                     break;
-                case Meses.Septiembre:
+                case 9:
+                    Mes = Meses.Septiembre.ToString();
                     break;
-                case Meses.Octubre:
+                case 10:
+                    Mes = Meses.Octubre.ToString();
                     break;
-                case Meses.Noviembre:
+                case 11:
+                    Mes = Meses.Noviembre.ToString();
                     break;
-                case Meses.Diciembre:
+                case 12:
+                    Mes = Meses.Diciembre.ToString();
                     break;
                 default:
+                    Console.WriteLine("\nEste mes no existe.");
                     break;
             }
-
             return Mes;
         }
-    }
-    
-    //Enumeración de los Meses
-    enum Meses
-    {
-        Enero,
-        Febrero,
-        Marzo,
-        Abril,
-        Mayo,
-        Junio,
-        Julio,
-        Agosto,
-        Septiembre,
-        Octubre,
-        Noviembre,
-        Diciembre
+
+        //Enumeración de los Meses
+        enum Meses
+        {
+            Enero,
+            Febrero,
+            Marzo,
+            Abril,
+            Mayo,
+            Junio,
+            Julio,
+            Agosto,
+            Septiembre,
+            Octubre,
+            Noviembre,
+            Diciembre
+        }
     }
 }
-
